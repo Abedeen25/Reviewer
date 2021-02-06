881ef91ccd820db946728315263738632ab13a72
Abedeen <iftekharul25abedeen@gmail.com>
2021-02-07 00:18:43 +0600

    Commenting and rating initiated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import ViewArea from './components/ViewArea';
 import { getBooksBySearch } from './Services/BooksAPI';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import DetailsPage from './Pages/DetailsPage';
+import firebase from 'firebase';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyC9C6ykMC9yyfl-wSmyewjpxzXuj0Jq2Fc",
+  authDomain: "reviewapp-a5975.firebaseapp.com",
+  projectId: "reviewapp-a5975",
+  storageBucket: "reviewapp-a5975.appspot.com",
+  messagingSenderId: "871884958406",
+  appId: "1:871884958406:web:d30f57be356a9663d085c7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -55,8 +68,6 @@ const App = () => {
           </Route>
         </div>
       </BrowserRouter>
-
-
 
     </div>
 
--- src/Pages/DetailsPage.js
+++ src/Pages/DetailsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import Rating from '@material-ui/lab/Rating';
 
 import {
     Card,
@@ -8,65 +9,99 @@ import {
     CardTitle,
     CardImg,
     CardBody,
-    CardFooter
+    CardFooter,
+    FormTextarea,
+    Button
 } from "shards-react";
 
 export default function DetailsPage(props) {
+    const [value, setValue] = React.useState(2);
+    const [Review, setReview] = React.useState('anytning');
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
-            <Card style={{ minWidth: '400px', maxWidth: '400px', margin: '2em', alignSelf: 'center' }}>
-                {(props.info.imageLinks === undefined) ?
-                    <CardImg
-                        top
-                        src="https://picsum.photos/450/650"
-                        style={{ width: 400, height: 650 }}
-                    /> : <CardImg
-                        top
-                        src={props.info.imageLinks.thumbnail}
-                        style={{ width: 400, height: 650 }}
-                    />}
+            <div>
+                <Card className="sticky-top" style={{ minWidth: '400px', maxWidth: '400px', margin: '2em', alignSelf: 'center', position: 'sticky' }}>
+                    {(props.info.imageLinks === undefined) ?
+                        <CardImg
+                            top
+                            src="https://picsum.photos/450/650"
+                            style={{ width: 400, height: 650 }}
+                        /> : <CardImg
+                            top
+                            src={props.info.imageLinks.thumbnail}
+                            style={{ width: 400, height: 650 }}
+                        />}
 
-                <CardBody>
+                    <CardBody style={{ background: 'white' }}>
 
-                    <CardTitle>{props.info.title}</CardTitle>
+                        <CardTitle>{props.info.title}</CardTitle>
 
-                </CardBody>
-                <CardFooter className='back'>
-                    <Link to='/'>{'< Go back to home'}</Link>
-                </CardFooter>
-            </Card>
+                    </CardBody>
+                    <CardFooter className='back'>
+                        <Link to='/'>{'< Go back to home'}</Link>
+                    </CardFooter>
+                </Card>
+            </div>
+            <div>
+                <Card style={{ maxWidth: '750px', margin: '2em 2em 2em 0', width: 'auto', justifySelf: 'center', alignSelf: 'center', }}>
+                    <CardHeader>Book Details</CardHeader>
+                    <CardBody>
 
-            <Card style={{ maxWidth: '750px', margin: '2em 2em 2em 0', width: 'auto', justifySelf: 'center', alignSelf: 'center', }}>
-                <CardHeader>Book Details</CardHeader>
-                <CardBody>
+                        {(props.info.authors !== undefined) ?
+                            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
 
-                    {(props.info.authors !== undefined) ?
-                        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                                <h6>Authors: </h6>
+                                {props.info.authors.map((author, i) => {
+                                    return (<p key={i}>{', ' + author} </p>)
+                                })}
 
-                            <h6>Authors: </h6>
-                            {props.info.authors.map((author, i) => {
-                                return (<p key={i}>{', ' + author} </p>)
-                            })}
+                            </div> : ''}
 
-                        </div> : ''}
+                        {(props.info.publisher !== undefined) ?
+                            <div style={{ display: 'flex' }}>
+                                <h6>Publisher </h6> <p>: {props.info.publisher}</p>
+                            </div> : ''}
 
-                    {(props.info.publisher !== undefined) ?
+                        {(props.info.publishedDate !== undefined) ?
+                            <div style={{ display: 'flex' }}>
+                                <h6>Publishing Date: </h6> <p>: {props.info.publishedDate}</p>
+                            </div> : ''}
+
+
                         <div style={{ display: 'flex' }}>
-                            <h6>Publisher </h6> <p>: {props.info.publisher}</p>
-                        </div> : ''}
+                            <h6>Details </h6>
+                            <p>: {props.info.description}</p>
+                        </div>
+                    </CardBody>
+                </Card>
+                <Card style={{ maxWidth: '750px', margin: '2em 2em 2em 0', width: 'auto', justifySelf: 'center', alignSelf: 'center', }}>
+                    <CardHeader style={{ display: 'flex' }}>
 
-                    {(props.info.publishedDate !== undefined) ?
-                        <div style={{ display: 'flex' }}>
-                            <h6>Publishing Date: </h6> <p>: {props.info.publishedDate}</p>
-                        </div> : ''}
+                        <div style={{ display: 'flex' }}>Give a Review</div>
 
+                        <div style={{ flex: 'auto' }}></div>
 
-                    <div style={{ display: 'flex' }}>
-                        <h6>Details </h6>
-                        <p>: {props.info.description}</p>
-                    </div>
-                </CardBody>
-            </Card>
+                        <div style={{ display: 'flex', }}>
+                            <Rating
+                                name="simple-controlled"
+                                value={value}
+                                onChange={(event, newValue) => {
+                                    setValue(newValue);
+                                }}
+                            /></div>
+
+                    </CardHeader>
+                    <CardBody>
+                        {Review}
+                        <FormTextarea onChange={(event) => { setReview(event.target.value) }}></FormTextarea>
+                        <div style={{ display: 'flex', margin: '2em 0 0 0' }}>
+                            <div style={{ flex: 'auto' }}></div>
+                            <Button>Post Review</Button>
+                        </div>
+                    </CardBody>
+                </Card>
+            </div>
         </div>
     );
 }
